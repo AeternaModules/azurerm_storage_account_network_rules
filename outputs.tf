d6c1213920a9ebcde47d5cc58bfd09ef508ac4fd
Daniel Allen <dan@aeternagroup.net>
2026-01-11 19:28:17 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "storage_account_network_ruless" {
+  description = "All storage_account_network_rules resources"
+  value       = azurerm_storage_account_network_rules.storage_account_network_ruless
+}
+
---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "storage_account_network_ruless" {
+  description = <<EOT
+Map of storage_account_network_ruless, attributes below
+Required:
+    - default_action
+    - storage_account_id
+Optional:
+    - bypass
+    - ip_rules
+    - virtual_network_subnet_ids
+    - private_link_access (block):
+        - endpoint_resource_id (required)
+        - endpoint_tenant_id (optional)
+EOT
+
+  type = map(object({
+    default_action             = string
+    storage_account_id         = string
+    bypass                     = optional(set(string))
+    ip_rules                   = optional(set(string))
+    virtual_network_subnet_ids = optional(set(string))
+    private_link_access = optional(object({
+      endpoint_resource_id = string
+      endpoint_tenant_id   = optional(string)
+    }))
+  }))
+}
+
