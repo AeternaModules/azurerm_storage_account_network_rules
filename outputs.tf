04a658f3466bdc11d4304cbc3bbd9d3750e3817a
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:45 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "storage_account_network_ruleses" {
-  description = "All storage_account_network_rules resources"
-  value       = azurerm_storage_account_network_rules.storage_account_network_ruleses
-}
 output "storage_account_network_ruleses_bypass" {
-  description = "List of bypass values across all storage_account_network_ruleses"
-  value       = [for k, v in azurerm_storage_account_network_rules.storage_account_network_ruleses : v.bypass]
+  description = "Map of bypass values across all storage_account_network_ruleses, keyed the same as var.storage_account_network_ruleses"
+  value       = { for k, v in azurerm_storage_account_network_rules.storage_account_network_ruleses : k => v.bypass }
 }
 output "storage_account_network_ruleses_default_action" {
-  description = "List of default_action values across all storage_account_network_ruleses"
-  value       = [for k, v in azurerm_storage_account_network_rules.storage_account_network_ruleses : v.default_action]
+  description = "Map of default_action values across all storage_account_network_ruleses, keyed the same as var.storage_account_network_ruleses"
+  value       = { for k, v in azurerm_storage_account_network_rules.storage_account_network_ruleses : k => v.default_action }
 }
 output "storage_account_network_ruleses_ip_rules" {
-  description = "List of ip_rules values across all storage_account_network_ruleses"
-  value       = [for k, v in azurerm_storage_account_network_rules.storage_account_network_ruleses : v.ip_rules]
+  description = "Map of ip_rules values across all storage_account_network_ruleses, keyed the same as var.storage_account_network_ruleses"
+  value       = { for k, v in azurerm_storage_account_network_rules.storage_account_network_ruleses : k => v.ip_rules }
 }
 output "storage_account_network_ruleses_private_link_access" {
-  description = "List of private_link_access values across all storage_account_network_ruleses"
-  value       = [for k, v in azurerm_storage_account_network_rules.storage_account_network_ruleses : v.private_link_access]
+  description = "Map of private_link_access values across all storage_account_network_ruleses, keyed the same as var.storage_account_network_ruleses"
+  value       = { for k, v in azurerm_storage_account_network_rules.storage_account_network_ruleses : k => v.private_link_access }
 }
 output "storage_account_network_ruleses_storage_account_id" {
-  description = "List of storage_account_id values across all storage_account_network_ruleses"
-  value       = [for k, v in azurerm_storage_account_network_rules.storage_account_network_ruleses : v.storage_account_id]
+  description = "Map of storage_account_id values across all storage_account_network_ruleses, keyed the same as var.storage_account_network_ruleses"
+  value       = { for k, v in azurerm_storage_account_network_rules.storage_account_network_ruleses : k => v.storage_account_id }
 }
 output "storage_account_network_ruleses_virtual_network_subnet_ids" {
-  description = "List of virtual_network_subnet_ids values across all storage_account_network_ruleses"
-  value       = [for k, v in azurerm_storage_account_network_rules.storage_account_network_ruleses : v.virtual_network_subnet_ids]
+  description = "Map of virtual_network_subnet_ids values across all storage_account_network_ruleses, keyed the same as var.storage_account_network_ruleses"
+  value       = { for k, v in azurerm_storage_account_network_rules.storage_account_network_ruleses : k => v.virtual_network_subnet_ids }
 }
 
