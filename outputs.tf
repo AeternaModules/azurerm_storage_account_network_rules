d47c10e7216c532c443e6cf677ff0597a3eb3afb
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:51:16 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,29 @@
 output "storage_account_network_rules_id" {
   description = "Map of id values across all storage_account_network_rules, keyed the same as var.storage_account_network_rules"
-  value       = { for k, v in azurerm_storage_account_network_rules.storage_account_network_rules : k => v.id }
+  value       = { for k, v in azurerm_storage_account_network_rules.storage_account_network_rules : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "storage_account_network_rules_bypass" {
   description = "Map of bypass values across all storage_account_network_rules, keyed the same as var.storage_account_network_rules"
-  value       = { for k, v in azurerm_storage_account_network_rules.storage_account_network_rules : k => v.bypass }
+  value       = { for k, v in azurerm_storage_account_network_rules.storage_account_network_rules : k => v.bypass if v.bypass != null && length(v.bypass) > 0 }
 }
 output "storage_account_network_rules_default_action" {
   description = "Map of default_action values across all storage_account_network_rules, keyed the same as var.storage_account_network_rules"
-  value       = { for k, v in azurerm_storage_account_network_rules.storage_account_network_rules : k => v.default_action }
+  value       = { for k, v in azurerm_storage_account_network_rules.storage_account_network_rules : k => v.default_action if v.default_action != null && length(v.default_action) > 0 }
 }
 output "storage_account_network_rules_ip_rules" {
   description = "Map of ip_rules values across all storage_account_network_rules, keyed the same as var.storage_account_network_rules"
-  value       = { for k, v in azurerm_storage_account_network_rules.storage_account_network_rules : k => v.ip_rules }
+  value       = { for k, v in azurerm_storage_account_network_rules.storage_account_network_rules : k => v.ip_rules if v.ip_rules != null && length(v.ip_rules) > 0 }
 }
 output "storage_account_network_rules_private_link_access" {
   description = "Map of private_link_access values across all storage_account_network_rules, keyed the same as var.storage_account_network_rules"
-  value       = { for k, v in azurerm_storage_account_network_rules.storage_account_network_rules : k => v.private_link_access }
+  value       = { for k, v in azurerm_storage_account_network_rules.storage_account_network_rules : k => v.private_link_access if v.private_link_access != null && length(v.private_link_access) > 0 }
 }
 output "storage_account_network_rules_storage_account_id" {
   description = "Map of storage_account_id values across all storage_account_network_rules, keyed the same as var.storage_account_network_rules"
-  value       = { for k, v in azurerm_storage_account_network_rules.storage_account_network_rules : k => v.storage_account_id }
+  value       = { for k, v in azurerm_storage_account_network_rules.storage_account_network_rules : k => v.storage_account_id if v.storage_account_id != null && length(v.storage_account_id) > 0 }
 }
 output "storage_account_network_rules_virtual_network_subnet_ids" {
   description = "Map of virtual_network_subnet_ids values across all storage_account_network_rules, keyed the same as var.storage_account_network_rules"
-  value       = { for k, v in azurerm_storage_account_network_rules.storage_account_network_rules : k => v.virtual_network_subnet_ids }
+  value       = { for k, v in azurerm_storage_account_network_rules.storage_account_network_rules : k => v.virtual_network_subnet_ids if v.virtual_network_subnet_ids != null && length(v.virtual_network_subnet_ids) > 0 }
 }
 
